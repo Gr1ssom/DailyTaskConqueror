45ae1f5d7f025ee5251a779085cef54983077221
Your Name <you@example.com>
2023-07-17 21:27:49 -0500

    changes to auth.js and tasks.js

---
+++ backend/routes/tasks.js
@@ -0,0 +1,63 @@
+// routes/taskRoutes.js
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/task');
+
+// Route to get all tasks
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Route to create a new task
+router.post('/tasks', async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Route to update a task
+router.patch('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    if (req.body.title) task.title = req.body.title;
+    if (req.body.description) task.description = req.body.description;
+    if (req.body.completed !== undefined) task.completed = req.body.completed;
+
+    const updatedTask = await task.save();
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Route to delete a task
+router.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndRemove(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json({ message: 'Task deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
