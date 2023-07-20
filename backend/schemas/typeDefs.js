2c621851dbc7a458dc530232e8a8ccac81fed018
Aubrey Gunn <agunn369@gmail.com>
2023-07-19 21:17:15 -0500

    file restructure and server changes

--- backend/schemas/typeDefs.js
+++ backend/schemas/typeDefs.js
@@ -39,3 +39,24 @@ type Tasks {
 `;
 
 module.exports = typeDefs;
+
+// const typeDefs = gql`
+//  type Task {
+    _id: ID!
+    title: String!
+    description: String
+    dueDate: String
+    completed: Boolean!
+//  }
+
+ // type Query {
+    tasks: [Task]
+    task(id: ID!): Task
+ // }
+
+ // type Mutation {
+//    createTask(title: String!, description: String, dueDate: String, completed: Boolean!): Task
+ //   updateTask(id: ID!, title: String, description: String, dueDate: String, completed: Boolean): Task
+ //   deleteTask(id: ID!): Task
+ // }
+//`;
